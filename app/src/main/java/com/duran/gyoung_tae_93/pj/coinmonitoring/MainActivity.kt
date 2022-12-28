b652caf29628e6d0332f5ffb17907f152ebeb4ca
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-12-28 16:42:52 +0900

    Start -> Coin Price Monitoring App Clone Coding start

---
+++ app/src/main/java/com/duran/gyoung_tae_93/pj/coinmonitoring/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.duran.gyoung_tae_93.pj.coinmonitoring
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
