837fdf492fca598e8de32b09d758901e0ab89f22
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-12-28 17:44:10 +0900

    Image Setting & Timber Log 찍기

---
+++ app/src/main/java/com/duran/gyoung_tae_93/pj/coinmonitoring/App.kt
@@ -0,0 +1,12 @@
+package com.duran.gyoung_tae_93.pj.coinmonitoring
+
+import android.app.Application
+import timber.log.Timber
+
+class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
--- app/src/main/java/com/duran/gyoung_tae_93/pj/coinmonitoring/MainActivity.kt
+++ app/src/main/java/com/duran/gyoung_tae_93/pj/coinmonitoring/MainActivity.kt
@@ -2,10 +2,17 @@ package com.duran.gyoung_tae_93.pj.coinmonitoring
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("MainActivity", "onCreate")
+        Timber.d("onCreate")
+        Timber.tag("MainActivity").d("onCreate")
+        Timber.tag("MainActivity").e("onCreate")
     }
 }
