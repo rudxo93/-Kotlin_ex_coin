83f0ab7cc6b168db66ba0b1db46935a15d417364
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2023-02-03 14:40:08 +0900

    메인 하단 탭 프래그먼트 연결(Bottom Navigation / Fragment)

--- app/src/main/java/com/duran/gyoung_tae_93/pj/coinmonitoring/view/main/MainActivity.kt
+++ app/src/main/java/com/duran/gyoung_tae_93/pj/coinmonitoring/view/main/MainActivity.kt
@@ -2,14 +2,28 @@ package com.duran.gyoung_tae_93.pj.coinmonitoring.view.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.duran.gyoung_tae_93.pj.coinmonitoring.R
+import com.duran.gyoung_tae_93.pj.coinmonitoring.databinding.ActivityMainBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         Timber.d("onCreate")
+
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bottomnavigationView = binding.bottomNavigationView
+        val navController = findNavController(R.id.fragmentContainerView)
+
+        bottomnavigationView.setupWithNavController(navController)
+
     }
 }
